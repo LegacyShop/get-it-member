d11ed26326ddccc8ef3f0e8a6359d5be670f37d2
xTimeStudio <69971638+xTimeStudio@users.noreply.github.com>
2020-12-19 20:51:32 +0500

    Update help.js

--- server/src/commands/general/help.js
+++ server/src/commands/general/help.js
@@ -70,7 +70,7 @@ module.exports = class HelpCommand extends Command {
     } else {
       embed.setDescription('If you need more help please ask for it on our support server: https://discord.gg/y4AFbt5\n\n**`NEW Economy` commands list:**\n`giveaways`: View list of giveaways discord channels.\n`store`: View how to buy coins and official store website link.\n`boost`: View how to get 100 coins by boosting `GetMembers+ [Support]` server.\n\n**`General` commands list:**\n`help`: Displays a list of available commands, or detailed information for a specified command.\n`invite`: Shows bot invite link.\n`prefix`: Shows or sets the command prefix.\n\n**`Economy` commands list:**\n`balance`: Shows user current balance.\n`find`: Join servers to receive ADV coins.\n`buy`: Buy a place in +find for ADV coins.\n`pay`: Transfer ADV coins to another user.\n`use`: Use a gift code.\n`daily`: Get daily coins. only for `GetMembers+ [Support]` server members.\n`current`: View current status of your advertises.\n`check`: View how many time left to no lose coins.\n`store`: Official store of coins + information how to buy coins.');
     }
-
+    message.reply('BUY COINS, 5 DOLLARS => 100 COINS. DO \`+store\`')
     message.embed(embed);
   }
 };
